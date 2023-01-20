dc094eb83ca01c3fad2034db8131b89578f77abb
Justin Joseph <justinjoseph2336@gmail.com>
2023-01-20 08:07:16 -0500

    second commit

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ *  WEB322 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Justin Joseph Student ID: 127690212 Date: 20/01/2023
+ *
+ *  Online (Cyclic) URL: https://weary-ring-worm.cyclic.app/
+ *
+ ********************************************************************************/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
